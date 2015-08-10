e30a2a222fe459c2d46c3557dc35b3de07360b98
alistefano <alex@asp-globe.com>
2015-08-10 19:18:31 +0200

    Init

---
+++ SvStackAuthTest/Global.asax.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Funq;
+using ServiceStack;
+using ServiceStack.Auth;
+using ServiceStack.Configuration;
+
+namespace SvStackAuthTest
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        public class GServiceHost : AppHostBase
+        {
+            public GServiceHost() : base("GService", typeof(TestService).Assembly)
+            {
+            }
+
+            public override void Configure(Container container)
+            {
+                Func<string, string> localize = s => HostContext.AppHost.ResolveLocalizedString(s, null);
+                Plugins.Add(new SessionFeature());
+                Config.AllowSessionIdsInHttpParams = true;
+
+                var appSettings = new AppSettings();
+               
+                var authFeature = new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] { new CredentialsAuthProvider{ SkipPasswordVerificationForInProcessRequests = true }, new FacebookAuthProvider(appSettings) }) { HtmlRedirect = null };
+                authFeature.IncludeAssignRoleServices = false;
+
+                Plugins.Add(new ServerEventsFeature()
+                {
+                    LimitToAuthenticatedUsers = true,
+                    NotifyChannelOfSubscriptions = false,
+                });
+                Plugins.Add(new RegistrationFeature());
+                Plugins.Add(authFeature);
+                container.Register<IUserAuthRepository>(new InMemoryAuthRepository());
+            }
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GServiceHost gHost = new GServiceHost();
+            gHost.Init();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SvStackAuthTest/TestService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+
+namespace SvStackAuthTest
+{
+    [Route("/test/{Msg}")]
+    public class TestRequest :IReturn<TestResponse>
+    {
+        public string Msg { get; set; }
+    }
+
+    public class TestResponse
+    {
+        public string Msg { get; set; }
+    }
+   
+    public class TestService : Service
+    {
+        [Authenticate]
+        public object Any(TestRequest req)
+        {
+            return new TestResponse() {Msg = req.Msg};
+        }
+    }
+}
